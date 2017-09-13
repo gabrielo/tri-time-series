90c47cd944f08f1118cf74882140b32e68644382
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2017-09-13 12:39:19 -0400

    Initial visualizaton scaffold

---
+++ src/index.js
@@ -0,0 +1,105 @@
+var triGl;
+var map;
+var gl;
+var canvasLayer;
+var mapMatrix = new Float32Array(16);
+var pixelsToWebGLMatrix = new Float32Array(16);
+var gui;
+var timeSlider;
+
+var mapOptions = {
+  zoom: 2,
+  center: new google.maps.LatLng(0.0, 0.0),
+  styles: mapStyles
+};
+
+var canvasLayerOptions = {
+  resizeHandler: resize,
+  animate: true,
+  updateHandler: update
+};
+
+function resize() {
+  var w = gl.canvas.width;
+  var h = gl.canvas.height;
+  gl.viewport(0, 0, w, h);
+  pixelsToWebGLMatrix.set([2/w, 0,   0, 0,
+    0,  -2/h, 0, 0,
+    0,   0,   0, 0,
+    -1,   1,   0, 1]);
+}
+
+function update() {
+  var mapProjection = map.getProjection();
+  mapMatrix.set(pixelsToWebGLMatrix);
+  var scale = canvasLayer.getMapScale();
+  scaleMatrix(mapMatrix, scale, scale);
+  var translation = canvasLayer.getMapTranslation();
+  translateMatrix(mapMatrix, translation.x, translation.y);  
+
+  var currentTime = timeSlider.getCurrentTime();
+  var currentDate = new Date(currentTime);
+  var currentYear = currentDate.getUTCFullYear();
+  var nextYear = currentYear + 1;
+  var startYear = new Date(currentYear + '-01-01');
+  var endYear = new Date(nextYear + '-01-01');
+  var alpha = (endYear.getTime() - currentTime)/(endYear.getTime() - startYear.getTime())
+  triGl.draw(mapMatrix, {currentYear: currentYear, alpha: 1.0 - alpha});
+  timeSlider.animate();
+}
+
+function initTimeSlider(opts) {
+  var startTime = new Date("1987-01-01").getTime();
+  var endTime = new Date("2016-12-31").getTime();
+  if (typeof(opts) != "undefined") {
+    if (opts.startTime) {
+      startTime = opts.startTime;
+    }
+    if (opts.endTime) {
+      endTime = opts.endTime;
+    }
+
+  }
+  var timeSlider = new TimeSlider({
+    startTime: startTime,
+    endTime: endTime,
+    dwellAnimationTime: 2 * 1000,
+    increment: 24*60*60*1000,
+    formatCurrentTime: function(date) {
+      //return date.yyyymmdd();
+      return date.getUTCFullYear();
+    },
+    animationRate: {
+      fast: 10,
+      medium: 20,
+      slow: 40
+    }
+  });  
+  return timeSlider;
+}
+
+function init() {
+  var mapDiv = document.getElementById('map-div');
+  var dataUrl = '../data/tri-time-series.bin';
+
+  map = new google.maps.Map(mapDiv, mapOptions);
+  canvasLayerOptions.map = map;
+  canvasLayer = new CanvasLayer(canvasLayerOptions);
+
+  timeSlider = initTimeSlider();
+
+  gl = canvasLayer.canvas.getContext('experimental-webgl');
+  gl.getExtension("OES_standard_derivatives");
+
+  triGl = new TriGl(gl);
+  triGl.getBin(dataUrl, function(data) {
+    triGl.setBuffer(data);
+  })
+
+
+  gui = new dat.GUI();
+  //gui.add(gtdGl, 'show0Casualties');
+
+ }
+
+document.addEventListener('DOMContentLoaded', init, false);
---
+++ src/tri-gl.js
@@ -0,0 +1,109 @@
+// tri-gl.js
+
+var TriVertexShader = 
+'attribute vec4 a_coord;\n' +
+'attribute float a_epoch_1;\n' +
+'attribute float a_epoch_2;\n' +
+'attribute float a_pbt_crc_1;\n' +
+'attribute float a_pbt_total_1;\n' +
+'attribute float a_pbt_crc_2;\n' +
+'attribute float a_pbt_total_2;\n' +
+'attribute float a_tri_crc_1;\n' +
+'attribute float a_tri_total_1;\n' +
+'attribute float a_tri_crc_2;\n' +
+'attribute float a_tri_total_2;\n' +
+'uniform mat4 u_map_matrix;\n' +
+'uniform float u_epoch;\n' +
+'uniform float u_alpha;\n' +
+'void main() {\n' +
+'    vec4 position;\n' +
+'    if (u_epoch < a_epoch_1) {\n' +
+'        position = vec4(-1,-1,-1,-1);\n' +
+'    } else if (u_epoch > a_epoch_2) {\n' +
+'        position = vec4(-1,-1,-1,-1);\n' +
+'    } else {\n' +
+'        position = u_map_matrix * a_coord;\n' +
+'    }\n' + 
+'    gl_Position = position;\n' +
+'    float pbt_delta = (a_pbt_total_2 - a_pbt_total_1) * u_alpha + a_pbt_total_1;\n' +
+'    float tri_delta = (a_tri_total_2 - a_tri_total_1) * u_alpha + a_tri_total_1;\n' +
+'    bool pbt_crc = a_pbt_crc_1 == 1.0 || a_pbt_crc_2 == 1.0;\n' + 
+'    bool tri_crc = a_tri_crc_1 == 1.0 || a_tri_crc_2 == 1.0;\n' + 
+'    gl_PointSize = clamp(sqrt(pbt_delta + tri_delta), 0.0, 50.0);\n' +
+'}\n';
+
+var TriFragmentShader = 
+'#extension GL_OES_standard_derivatives : enable\n' +
+'precision mediump float;\n' +
+'void main() {\n' +
+'    vec4 color = vec4(1.0,0.0,0.0,1.0);\n' +
+'    gl_FragColor = color;\n' + 
+'}\n';
+
+var TriGl = function TriGl(gl) {
+    this.gl = gl;
+    this.program = createProgram(gl, TriVertexShader, TriFragmentShader);
+    this.buffer = {
+        'numAttributes': 12,
+        'count': 0,
+        'buffer': null,
+        'ready': false
+    };
+    this.show0Casualties = true;
+
+}
+
+TriGl.prototype.getBin = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'arraybuffer';
+    xhr.open('get', url, true);
+    xhr.onload = function () {
+      var float32Array = new Float32Array(this.response);
+      callback(float32Array);
+    };
+    xhr.send();
+}
+
+
+TriGl.prototype.setBuffer = function(data) {
+    this.data = data;
+    this.buffer.count = data.length / this.buffer.numAttributes;
+    this.buffer.buffer = createBuffer(gl, data);   
+    this.buffer.ready = true;
+}
+
+TriGl.prototype.draw = function draw(transform, options) {
+    if (this.buffer.ready) {
+        var options = options || {};
+        var gl = this.gl;
+        gl.enable(gl.BLEND);
+        gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
+        var program = this.program;
+        var buffer = this.buffer;
+        gl.useProgram(program.program);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer.buffer);
+        bindAttribute(gl, program.program, 'a_coord', 2, gl.FLOAT, false, this.buffer.numAttributes*4, 0);    
+        bindAttribute(gl, program.program, 'a_epoch_1', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 8);    
+        bindAttribute(gl, program.program, 'a_epoch_2', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 12);    
+        bindAttribute(gl, program.program, 'a_pbt_crc_1', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 16);    
+        bindAttribute(gl, program.program, 'a_pbt_total_1', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 20);    
+        bindAttribute(gl, program.program, 'a_pbt_crc_2', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 24);    
+        bindAttribute(gl, program.program, 'a_pbt_total_2', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 28);    
+        bindAttribute(gl, program.program, 'a_tri_crc_1', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 32);    
+        bindAttribute(gl, program.program, 'a_tri_total_1', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 36);    
+        bindAttribute(gl, program.program, 'a_tri_crc_2', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 40);    
+        bindAttribute(gl, program.program, 'a_tri_total_2', 1, gl.FLOAT, false, this.buffer.numAttributes*4, 44);    
+
+        gl.uniformMatrix4fv(program.u_map_matrix, false, transform);
+        //gl.uniform1f(program.u_point_size, pointSize);
+
+        var currentEpoch = options.currentYear;
+        gl.uniform1f(program.u_epoch, currentEpoch);
+
+        var alpha = options.alpha;
+        gl.uniform1f(program.u_alpha, alpha);
+
+        gl.drawArrays(gl.POINTS, 0, buffer.count);
+        gl.disable(gl.BLEND);
+    }
+};
